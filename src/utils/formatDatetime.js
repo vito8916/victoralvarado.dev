76b2130b9cc39f3b9bb5a3a1d62c0eadf0ca93de
Victor Alvarado <vito8916@gmail.com>
2023-03-23 03:15:05 -0600

    First commit

--- src/utils/formatDatetime.js
+++ src/utils/formatDatetime.js
@@ -1,5 +1,4 @@
 const formatDatetime = (datetime) => {
-    const myDatetime = new Date(datetime);
     const d = new Date(datetime);
     return d.toLocaleDateString('en-US', {
         year: 'numeric',
