779a409282f12598ee57388ee2f454c8bfe64aec
Victor Alvarado <vito8916@gmail.com>
2023-03-23 14:16:12 -0600

    improve the Setup page.

---
+++ src/data/setupHardware.ts
@@ -0,0 +1,22 @@
+export const setupHardware = [
+    {
+        name: 'MacBook Pro 16 inch 2019',
+        url: 'https://a.co/d/eflpSRp',
+        description: 'Buy on Amazon a MacBook Pro 16 inch 2019.'
+    },
+    {
+        name: 'ASUS ProArt PA34VC 34" QHD Ultra Wide 1440p Curved Monitor',
+        url: 'https://a.co/d/3xifsUu',
+        description: 'Buy on Amazon a ASUS ProArt PA34VC 34 inch QHD Ultra Wide 1440p Curved Monitor.'
+    },
+    {
+        name: 'KNODEL Desk Mat, Dual-Sided Office Desk Pad, Mouse Pad',
+        url: 'https://a.co/d/bIUiW5K',
+        description: 'Buy on Amazon a Desk pad.'
+    },
+    {
+        name: 'OMOTON Vertical Laptop Stand, Double Desktop Stand Holder with Adjustable Dock',
+        url: 'https://a.co/d/f4z4tJB',
+        description: 'Buy on Amazon a OMOTON Vertical Laptop Stand, Double Desktop Stand Holder with Adjustable Dock.'
+    }
+]
---
+++ src/data/setupSoftware.ts
@@ -0,0 +1,17 @@
+export const setupSoftware = [
+    {
+        name: 'iTerm 2',
+        url: 'https://www.iterm2.com/',
+        description: 'Install iTerm 2 on your computer.'
+    },
+    {
+        name: 'WebStorm',
+        url: 'https://www.jetbrains.com/webstorm/',
+        description: 'Install WebStorm on your computer.'
+    },
+    {
+        name: 'Figma',
+        url: 'https://www.figma.com/',
+        description: 'Install Figma on your computer.'
+    }
+]
